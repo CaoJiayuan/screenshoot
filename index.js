b2177b266c05b9c7d56a2c4d78746ff68e081d13
cjy <cjy632258@hotmail.com>
2018-04-04 16:23:56 +0800

    update

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ app.get('/', function (req, res) {
         args: ['--no-sandbox', '--headless', '--disable-gpu']
     }).then(browser => {
         browser.newPage().then(page => {
-            page.setViewport({width:1360,height:768}).then(() => page.goto(req.query.url).then(p => {
+            let width = parseInt(req.query.width) || 1360
+            let height = parseInt(req.query.height) || 768
+            page.setViewport({width:width,height:height}).then(() => page.goto(req.query.url).then(p => {
                 console.log(`Shooting... [${req.query.url}] `);
-                page.title().then(title => page.screenshot({path: `./shoots/${title}.png`}).then(re => browser.close()))
+                page.title().then(title => {
+                    let name = req.query.name || title
+
+                    page.screenshot({path: `./shoots/${name}.png`}).then(re => browser.close())
+                })
             }))
         })
     });
     res.send('Hello World')
 })
 
-app.listen(3000, () => console.log('Server on'))
+app.listen(3003, () => console.log('Server on'))
